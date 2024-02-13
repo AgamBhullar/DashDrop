b1304819fa44a051d8153139a06f1fb7cce614c3
bhullaragam <asbhullar@ucdavis.edu>
2024-02-13 11:07:13 -0800

    Initial Commit

---
+++ DashDrop/DashDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashDropApp.swift
+//  DashDrop
+//
+//  Created by Agam Bhullar on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct DashDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
