c6287c10dbb2ffc238903d9b271302ef15f58108
AgamBhullar <asbhullar@ucdavis.edu>
2024-02-19 02:21:41 -0800

    -Integrated a seprate HomeView for better code readability. -Fixed the address suggestions issue. -Implemented a scroll feature that dismisses the keyboard when the user scrolls the suggested address list -Removed unnecessary files for address search and map view.

---
+++ DashDrop/View/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  DashDrop
+//
+//  Created by Agam Bhullar on 2/19/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
