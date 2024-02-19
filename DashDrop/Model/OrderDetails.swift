fbfaceec0952106ce2b40d2e6245966b1fd8dd04
AgamBhullar <asbhullar@ucdavis.edu>
2024-02-18 23:52:54 -0800

    Implemented zap and Imgur Api services to send users' created order info to the company's email.

---
+++ DashDrop/Model/OrderDetails.swift
@@ -0,0 +1,18 @@
+//
+//  OrderDetails.swift
+//  DashDrop
+//
+//  Created by Agam Bhullar on 2/14/24.
+//
+
+import SwiftUI
+
+struct OrderDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderDetails()
+}
