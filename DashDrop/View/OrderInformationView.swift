6e0909a1eb56359eb8db24bc01acf4442d2c4757
Harpreetpb65 <hsbasota@ucdavis.edu>
2024-02-29 21:37:07 -0800

    Added OrderInformation for user interface updted design on store selection and Homeview

---
+++ DashDrop/View/OrderInformationView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderInformationView.swift
+//  DashDrop
+//
+//  Created by Harpreet Basota on 2/25/24.
+//
+
+import SwiftUI
+
+struct OrderInformationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderInformationView()
+}
