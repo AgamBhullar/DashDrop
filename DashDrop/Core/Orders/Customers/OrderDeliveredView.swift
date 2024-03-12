87dab434855b435a226b369c3730d4eb1c8fc90d
AgamBhullar <asbhullar@ucdavis.edu>
2024-03-12 14:03:03 -0700

    Implemented DriverDeliverView and OrderDeliveredView Implemented a functionality to view the qrcode image uploaded by the customer on driver portal Implemented a functionality where the driver won't be able to confirm delivery unless they upload an image of the delivery receipt

---
+++ DashDrop/Core/Orders/Customers/OrderDeliveredView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderDeliveredView.swift
+//  DashDrop
+//
+//  Created by Agam Bhullar on 3/12/24.
+//
+
+import SwiftUI
+
+struct OrderDeliveredView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderDeliveredView()
+}
---
+++ DashDrop/Core/Orders/Drivers/DriverDeliveredView.swift
@@ -0,0 +1,18 @@
+//
+//  DriverDeliveredView.swift
+//  DashDrop
+//
+//  Created by Agam Bhullar on 3/12/24.
+//
+
+import SwiftUI
+
+struct DriverDeliveredView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DriverDeliveredView()
+}
