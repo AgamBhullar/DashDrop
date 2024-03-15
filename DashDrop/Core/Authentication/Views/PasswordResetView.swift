fcf4b9c7a6bd7103ad26eece84216b60a3e1a1df
AgamBhullar <asbhullar@ucdavis.edu>
2024-03-15 11:51:50 -0700

    Basota: Added a forget password functionality

---
+++ DashDrop/Core/Authentication/Views/PasswordResetView.swift
@@ -0,0 +1,18 @@
+//
+//  PasswordResetView.swift
+//  DashDrop
+//
+//  Created by Agam Bhullar on 3/15/24.
+//
+
+import SwiftUI
+
+struct PasswordResetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PasswordResetView()
+}
