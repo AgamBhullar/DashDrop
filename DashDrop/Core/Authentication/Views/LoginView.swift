3701ace30e4e52b26e918d733f555ce1b771efdc
AgamBhullar <asbhullar@ucdavis.edu>
2024-03-15 12:40:16 -0700

    Final Push

--- DashDrop/Core/Authentication/Views/LoginView.swift
+++ DashDrop/Core/Authentication/Views/LoginView.swift
@@ -18,127 +18,136 @@ struct LoginView: View {
             ZStack {
                 Color(.black)
                     .ignoresSafeArea()
-                
-                // Add tap gesture here to dismiss keyboard
-                                Color.clear
-                    .onTapGesture {
-                        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-                    }
-                                
-                VStack {
-                    
-                    // image and title
-                    
+                ScrollView(.vertical, showsIndicators: false) {
                     VStack {
-                        //image
-                        Image("Logo")
-                            .resizable()
-                            .frame(width: 300, height: 250)
-                    }
-                    
-                    // input fields
-                    VStack(spacing: 32) {
-                        //input field 1
-                        CustomInputField(text: $email, title: "Email Address", placeholder: "name@example.com")
-                            .autocapitalization(.none)
-                        //input field 2
+                        // image and title
                         
-                        CustomInputField(text: $password, title: "Password", placeholder: "Enter your password",
-                                         isSecureField: true)
-                        
-                        Button {
-                            
-                        } label: {
-                            Text("Forgot Password?")
-                                .font(.system(size: 13, weight: .semibold))
-                                .foregroundColor(.white)
-                                .padding(.top)
-                        }
-                        .frame(maxWidth: .infinity, alignment: .trailing)
-                    }
-                    .padding(.horizontal)
-                    .padding(.top, 12)
-                
-                    //social sign in view
-                    VStack {
-                        //divider + text
-                        HStack(spacing: 24) {
-                            Rectangle()
-                                .frame(width: 76, height: 1)
-                                .foregroundColor(.white)
-                                .opacity(0.5)
-                            
-                            Text("Sign in with social")
-                                .foregroundColor(.white)
-                                .fontWeight(.semibold)
-                            
-                            Rectangle()
-                                .frame(width: 76, height: 1)
-                                .foregroundColor(.white)
-                                .opacity(0.5)
+                        VStack {
+                            Image("Logo")
+                                .resizable()
+                                .frame(width: 300, height: 250)
                         }
                         
-                        //sign up buttons
-                        HStack {
+                        // input fields
+                        
+                        VStack(spacing: 22) {
+                            //input field 1
+                            CustomInputField(text: $email, title: "Email Address", placeholder: "name@example.com")
+                                .autocapitalization(.none)
+                            //input field 2
+                            
+                            CustomInputField(text: $password, title: "Password", placeholder: "Enter your password",
+                                             isSecureField: true)
+                            
                             Button {
                                 
                             } label: {
-                                Image("google-sign-in-icon")
-                                    .resizable()
-                                    .frame(width: 44, height: 44)
+                                Text("Forgot Password?")
+                                    .font(.system(size: 13, weight: .semibold))
+                                    .foregroundColor(.white)
+                                    .padding(.top)
+                            }
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                        }
+                        .padding(.horizontal)
+                        .padding(.bottom, 20)
+                        
+                        //social sign in view
+                        VStack {
+                            //divider + text
+                            HStack(spacing: 24) {
+                                Rectangle()
+                                    .frame(width: 76, height: 1)
+                                    .foregroundColor(.white)
+                                    .opacity(0.5)
+                                
+                                Text("Sign in with social")
+                                    .foregroundColor(.white)
+                                    .fontWeight(.semibold)
+                                
+                                Rectangle()
+                                    .frame(width: 76, height: 1)
+                                    .foregroundColor(.white)
+                                    .opacity(0.5)
+                            }
+                            
+                            //sign up buttons
+                            HStack {
+                                Button {
+                                    
+                                } label: {
+                                    Image("google-sign-in-icon")
+                                        .resizable()
+                                        .frame(width: 44, height: 44)
+                                }
                             }
                         }
-                    }
-                    .padding(.vertical)
-                    
-                    Spacer()
-                    
-                    //sign in button
-                    Button {
-                        viewModel.signIn(withEmail: email, password: password)
-                    } label: {
-                        HStack {
-                            Text("SIGN IN")
-                                .foregroundColor(.black)
-                            
-                            Image(systemName: "arrow.right")
-                                .foregroundColor(.black)
+                        .padding(.vertical)
+                        
+                        Spacer()
+                        
+                        //sign in button
+                        Button {
+                            viewModel.signIn(withEmail: email, password: password)
+                        } label: {
+                            HStack {
+                                Text("SIGN IN")
+                                    .foregroundColor(.black)
+                                
+                                Image(systemName: "arrow.right")
+                                    .foregroundColor(.black)
+                            }
+                            .frame(width: UIScreen.main.bounds.width - 32, height: 50)
+                            .scaleEffect(animate ? 1.1 : 1.0)
                         }
-                        .frame(width: UIScreen.main.bounds.width - 32, height: 50)
-                        .scaleEffect(animate ? 1.1 : 1.0)
-                    }
-                    .background(Color("CustomColor1"))
-                    .disabled(!formIsValid)
-                    .opacity(formIsValid ? 1.0 : 0.5)
-                    .cornerRadius(10)
-                    .padding(.top, 24)
-                    .onAppear {
-                        withAnimation(.easeInOut(duration: 2).repeatForever(autoreverses: true)) {
-                            animate = true
+                        .background(Color("CustomColor1"))
+                        .disabled(!formIsValid)
+                        .opacity(formIsValid ? 1.0 : 0.5)
+                        .cornerRadius(10)
+                        .padding(.top, 24)
+                        .onAppear {
+                            withAnimation(.easeInOut(duration: 2).repeatForever(autoreverses: true)) {
+                                animate = true
+                            }
                         }
-                    }
-
-                    
-                    //sign up button
-                    
-                    Spacer()
-                    
-                    NavigationLink {
-                        RegistrationView()
-                            .navigationBarBackButtonHidden(true)
-                    } label: {
-                        HStack {
-                            Text("Don't have an account?")
-                                .font(.system(size: 14))
-                            
-                            Text("Sign Up")
-                                .font(.system(size: 14, weight: .semibold))
+                        
+                        
+                        //sign up button
+                        
+                        Spacer()
+                        
+                        NavigationLink {
+                            RegistrationView()
+                                .navigationBarBackButtonHidden(true)
+                        } label: {
+                            HStack {
+                                Text("Don't have an account?")
+                                    .font(.system(size: 14))
+                                
+                                Text("Sign Up")
+                                    .font(.system(size: 14, weight: .semibold))
+                            }
+                            .foregroundColor(.white)
                         }
-                        .foregroundColor(.white)
+                        
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(Color.clear.onTapGesture {
+                            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                        })
+                        
                     }
+                }
+            
+                
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    self.hideKeyboard()
                 }
             }
         }
+    }
+    private func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
 
@@ -156,8 +165,3 @@ struct LoginView_Previews: PreviewProvider {
         LoginView()
     }
 }
-
-
-
-
-
