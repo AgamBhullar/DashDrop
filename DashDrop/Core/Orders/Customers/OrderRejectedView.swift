9408b8e2986ea70a80f0d556d592ed576e064c25
AgamBhullar <asbhullar@ucdavis.edu>
2024-03-14 22:05:18 -0700

    Implemented a function to show the order is completed and set the driver and the customer state to its original state. Fixed issue in the Order Details page to display the correct receipt assossiated to the order.

---
+++ DashDrop/Core/Orders/Customers/OrderRejectedView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderRejectedView.swift
+//  DashDrop
+//
+//  Created by Agam Bhullar on 3/14/24.
+//
+
+import SwiftUI
+
+struct OrderRejectedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderRejectedView()
+}
