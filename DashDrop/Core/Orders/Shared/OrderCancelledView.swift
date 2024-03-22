5c5531d44e486a2c166543db2814d4b0de31819e
AgamBhullar <asbhullar@ucdavis.edu>
2024-03-22 14:53:02 -0700

    Cleaned up the code and integratred cancel order functionality for both the customer and the driver

---
+++ DashDrop/Core/Orders/Shared/OrderCancelledView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderCancelledView.swift
+//  DashDrop
+//
+//  Created by Agam Bhullar on 3/22/24.
+//
+
+import SwiftUI
+
+struct OrderCancelledView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderCancelledView()
+}
